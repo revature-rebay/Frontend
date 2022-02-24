e7c4d198a999949bbd4fdc8098cc6e40eb1ad5b9
Elias Calagiu <eliasc81@gmail.com>
2022-02-24 14:57:12 -0800

    login and register functionality complete

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,21 +1,21 @@
 export class User {
 
-    public id:number;
-    public username:string;
-    public password:string; 
-    public email:string;
-    public firstName:string; 
-    public lastName:string; 
-    public roleId:number; 
-    
-    constructor(obj?:User){
+    public id: number;
+    public userName: string;
+    public passWord: string;
+    public email: string;
+    public firstName: string;
+    public lastName: string;
+    public roleId: number;
 
-            this.id = obj?.id ?? 0
-            this.username = obj?.username ?? ""
-            this.password = obj?.password ?? ""
-            this.email = obj?.email ?? ""
-            this.firstName = obj?.firstName ?? ""
-            this.lastName = obj?.lastName ?? ""
-            this.roleId = obj?.roleId ?? 0
+    constructor(obj?: User) {
+
+        this.id = obj?.id ?? 0
+        this.userName = obj?.userName ?? ""
+        this.passWord = obj?.passWord ?? ""
+        this.email = obj?.email ?? ""
+        this.firstName = obj?.firstName ?? ""
+        this.lastName = obj?.lastName ?? ""
+        this.roleId = obj?.roleId ?? 0
     }
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -8,35 +8,35 @@ import { User } from '../models/user';
 })
 export class LoginService {
 
-  url:string = "http://localhost:8080/";
-  show:boolean = false;
+  url: string = "http://localhost:8080/";
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  registerUser(user:User){
+  registerUser(user: User) {
 
     return this.http.post(this.url + "user", user);
 
   }
 
-  login(user:User){
-    
-    return this.http.post<User>(this.url +"user/login", user, {withCredentials:true});
+  login(user: User) {
+    console.log(user);
+
+    return this.http.post<User>(this.url + "user/login", user, { withCredentials: true });
 
   }
 
-  logout(){
+  logout() {
 
-    return this.http.post(this.url+"user/logout", null, {withCredentials:true});
+    return this.http.post(this.url + "user/logout", null, { withCredentials: true });
 
   }
 
-  getCurrentUser(){
-    return this.http.get<User>(this.url+"user/current", {withCredentials:true});
+  getCurrentUser() {
+    return this.http.get<User>(this.url + "user/current", { withCredentials: true });
   }
 
-  getUserById(id:number){
-    return this.http.get<User>(this.url+"user/"+id, {withCredentials:true});
+  getUserById(id: number) {
+    return this.http.get<User>(this.url + "user/" + id, { withCredentials: true });
   }
 
 }
