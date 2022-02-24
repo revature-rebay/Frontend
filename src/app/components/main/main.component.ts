4a1d64825da56924c12202771eb468c2381f9024
Eric Florence <ericflorence.dev@gmail.com>
2022-02-23 17:49:55 -0700

    added quantity incrementer html/css/ts to product listing

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -12,4 +12,31 @@ export class MainComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
+
+
+
+
+
+
+
+  
+  // quanity incrementer 
+  quantity:number = 1;
+  i=1;
+  plus(){
+    if(this.i < 100){
+      this.i++;
+      this.quantity = this.i;
+    }
+  }
+  minus(){
+    if(this.i != 1){
+      this.i--;
+      this.quantity = this.i;
+    }
+  }
+  // quanity incrementer
+
 }
