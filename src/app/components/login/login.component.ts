0d49a10d0b7fe6b2680ce13e63dd8bea1cdf60f6
Eric Florence <ericflorence.dev@gmail.com>
2022-02-24 18:35:35 -0700

    cleanup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -71,5 +71,4 @@ export class LoginComponent implements OnInit {
       }
     })
   }
-
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { User } from '../models/user';
 
 @Injectable({
