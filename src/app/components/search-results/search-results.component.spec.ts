2543b2955e2d94ea8283a0fe67b4eb8a235fd5a1
Elias Calagiu <eliasc81@gmail.com>
2022-03-02 08:43:37 -0800

    added imports to successfully test in login, searchresults, and productdetails component

--- src/app/components/search-results/search-results.component.spec.ts
+++ src/app/components/search-results/search-results.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SearchResultsComponent } from './search-results.component';
 
 describe('SearchResultsComponent', () => {
@@ -8,7 +9,8 @@ describe('SearchResultsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchResultsComponent ]
+      declarations: [ SearchResultsComponent ],
+      imports: [RouterTestingModule, HttpClientTestingModule]
     })
     .compileComponents();
   });
