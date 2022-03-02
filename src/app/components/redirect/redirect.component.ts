57adacbce4b735473547f14792e2e05bb52c0b83
rfloyd01 <lynrdskynrd05@gmail.com>
2022-03-02 14:14:58 -0500

    added comment to redirect component

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -11,6 +11,11 @@ export class RedirectComponent implements OnInit {
   constructor(private router:Router) { }
 
   ngOnInit(): void {
+    //I was having trouble getting the search button (which is located inside of
+    //the navbar component) getting to reload the search page when the button
+    //was clicked while already on the search page. Clicking the search button
+    //now loads of this 'redirect' component first which currently only serves 
+    //to redirect to the 'searc_results' page
     this.router.navigateByUrl("search_results");
   }
 
