2ad10543b4982b2c8674431c421bafa37b07a16a
michael <mludwig0626@aol.com>
2022-03-02 14:28:06 -0500

    Click for product details, Discount check added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [{
   path: 'login',
   component: LoginComponent
 }, {
-  path: 'product_details_page',
+  path: 'main/product_details_page/:productId',
   component: ProductDetailsPageComponent
 },{
   path: 'sales',
